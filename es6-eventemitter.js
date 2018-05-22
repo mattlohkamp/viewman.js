a7ac7d10983036f047215d6a94c97a95c0608572
mattlohkamp <work@mattlohkamp.com>
2018-05-21 18:05:27 -0700

    added autoplay feature
    also shored up some stuff, sensible defaults and invalid input checks

--- es6-eventemitter.js
+++ es6-eventemitter.js
@@ -1,33 +1,36 @@
-'use strict'; //  adapted from Dave Atchley: http://www.datchley.name/es6-eventemitter/
+ //  adapted from Dave Atchley: http://www.datchley.name/es6-eventemitter/
+
+'use strict'
 
 class CustomEvent {
-  constructor(type,target){
+  constructor(type, target){
     this.type = type
     this.target = target
     this.defaultPrevented = false
   }
-  preventDefault() {
-    this.defaultPrevented = true
+  preventDefault() {  this.defaultPrevented = true  }
+}
+
+class CustomEventListener {
+  constructor(label, callback, once=false){
+    this.label = label
+    this.callback = callback
+    this.once = once
   }
 }
 
 class EventEmitter {
-  constructor() { this.listeners = new Map(); }
-  addEventListener(label, callback) {
-    this.listeners.has(label) || this.listeners.set(label, []);
-    this.listeners.get(label).push(callback);
+  constructor() { this.listeners = new Map() }
+  addEventListener(label, callback, once=false) {
+    this.listeners.has(label) || this.listeners.set(label, [])
+    this.listeners.get(label).push(new CustomEventListener(label, callback, once))
   }
   removeEventListener(label, callback) {
       let listeners = this.listeners.get(label),
-          index;
+          index
 
       if (listeners && listeners.length) {
-          index = listeners.reduce((i, listener, index) => {
-              return (typeof obj == 'function' && listener === callback) ?
-                  i = index :
-                  i;
-          }, -1);
-
+          index = listeners.reduce((i, listener, index)=>(typeof obj == 'function' && listener.callback === callback) ? i = index : i, -1)
           if (index > -1) {
               listeners.splice(index, 1)
               this.listeners.set(label, listeners)
@@ -36,13 +39,14 @@ class EventEmitter {
       }
       return false
   }
-  emitEvent(label, ...args) {
-      let listeners = this.listeners.get(label);
+  dispatchEvent(label, ...args) {
+      let listeners = this.listeners.get(label)
       let e = new CustomEvent(label,this)
       if (listeners && listeners.length) {
-          listeners.forEach((listener) => {
-              listener(e, ...args)
-          });
+          listeners.forEach(listener=>{
+            listener.callback(e, ...args)
+            if(listener.once == true) listeners.splice(listeners.indexOf(listener), 1)
+          })
           return e
       }
       return false
--- viewman.js
+++ viewman.js
@@ -1,32 +1,32 @@
 'use strict'
 const ViewManOptions = {
-  init:0  //  uint, index of initial viewstate
+  init:0,  //  uint, index of initial viewstate
+  autoplay:true //  boolean, whether toView is immediately called at the end of instanciation
 }
 class ViewMan extends EventEmitter {
-  constructor(_views,_options) { super()
+  constructor(_views=[],_options={}) { super()
     this.views = _views
     for(let i = 0; i < this.views.length; i++)  { this.hide(i) }
-    this.options = Object.assign(ViewManOptions, _options)
-    this.currentIndex = this.options.init
-    this.toView(this.currentIndex)
+    this.options = Object.assign((_options instanceof Object) ? _options : {}, ViewManOptions)
+    this.currentIndex = (Number.isInteger(this.options.init) && Math.sign(this.options.init) > -1 && this.views.length > this.options.init) ? this.options.init : ViewManOptions.init
+    if(this.options.autoplay !== false)  this.toView(this.currentIndex)
   }
   toView(nextIndex){
-    if(this.emitEvent('viewChange').defaultPrevented != true){
+    if(this.dispatchEvent('viewChange').defaultPrevented !== true){
         this.hide(this.currentIndex)
         this.currentIndex = nextIndex
         this.show(this.currentIndex)
-      this.emitEvent('viewChanged')
+      this.dispatchEvent('viewChanged')
     }
+    //  return this.
   }
   hide(index){  this.showhide('hide',this.views[index]) }
   show(index){  this.showhide('show',this.views[index]) }
   showhide(state,viewState){
     if(viewState instanceof HTMLElement){
-      if(this.emitEvent('view'+state[0].toUpperCase() + state.slice(1)).defaultPrevented != true) viewState.style.display = (state==='show') ? null : 'none'
+      if(this.dispatchEvent('view'+state[0].toUpperCase() + state.slice(1)).defaultPrevented != true) viewState.style.display = (state==='show') ? null : 'none'
     }else if(viewState.length){
-      viewState.forEach(view=>{
-        this.showhide(state,view)
-      })
+      viewState.forEach(view=>this.showhide(state,view))
     }
   }
 }
